354a583a1f8939d63c297763dc44ab6c7f2449d8
takahiro <linlonghao61@gmail.com>
2023-09-27 18:13:54 +0900

    トップ画面を出力するビュー作成

--- src/main/java/servlet/WelcomeServlet.java
+++ src/main/java/servlet/WelcomeServlet.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Welcome")
-public class Welcome extends HttpServlet {
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
